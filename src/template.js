433e5036fec7bdcc5b43a5b7d669ee127e041489
Mer2022 <112989286+Mer2022@users.noreply.github.com>
2023-02-27 14:36:47 -0600

    added createIntern and createEngineer function

--- src/template.js
+++ src/template.js
@@ -16,4 +16,40 @@ const createTeam = (team) => {
       </div>
           `;
     };
+
+    const createEngineer = (engineer) => {
+        return `
+            <div class="card employee-card">
+        <div class="card-header">
+            <h2 class="card-title"></h2>
+            <h3 class="card-title"><i class="fas fa-glasses mr-2"></i></h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID: </li>
+                <li class="list-group-item">Email: <a href="mailto:"></a></li>
+                <li class="list-group-item">GitHub: <a href="https://github.com/" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
+            </ul>
+        </div>
+    </div>
+            `;
+      };
 }
+// create the html for interns
+const createIntern = (intern) => {
+    return `
+        <div class="card employee-card">
+    <div class="card-header">
+        <h2 class="card-title"></h2>
+        <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i></h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: </li>
+            <li class="list-group-item">Email: <a href="mailto:"></a></li>
+            <li class="list-group-item">School: </li>
+        </ul>
+    </div>
+</div>
+        `;
+  };
